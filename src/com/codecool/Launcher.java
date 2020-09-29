01ad1936c4721cbf95d3c4d02eccfca898fe6db4
Viktor Bocz <beviktor95@gmail.com>
2020-09-29 11:14:15 +0200

    Removed unnecessary codesnippet

--- src/com/codecool/Launcher.java
+++ src/com/codecool/Launcher.java
@@ -1,11 +1,50 @@
 package com.codecool;
 
-import com.codecool.state.Context;
-import com.codecool.state.StartState;
-import com.codecool.state.StopState;
+import java.util.Collections;
+import java.util.List;
 
 public class Launcher {
     public static void main(String[] args) {
 
+    }
+
+    public static List<Integer> bubbleSort(List<Integer> listToBeSorted) {
+        for (int i = 0; i < listToBeSorted.size(); i++) {
+            for (int j = 0; j < listToBeSorted.size() - 1; j++) {
+                if (listToBeSorted.get(j) > listToBeSorted.get(j + 1)) {
+                    Collections.swap(listToBeSorted, j, j + 1);
+                }
+            }
+        }
+        return listToBeSorted;
+    }
+
+    public static int findMinInList(List<Integer> listWithDigits){
+        Integer minimumValue = listWithDigits.get(0);
+        for (Integer listWithDigit : listWithDigits) {
+            if (listWithDigit < minimumValue) {
+                minimumValue = listWithDigit;
+            }
+        }
+        return minimumValue;
+    }
+
+    public static int findMaxInList(List<Integer> listWithDigits){
+        Integer maximumValue = listWithDigits.get(0);
+        for (Integer listWithDigit : listWithDigits) {
+            if (listWithDigit > maximumValue) {
+                maximumValue = listWithDigit;
+            }
+        }
+        return maximumValue;
+    }
+
+    public static float calculateAvg(List<Integer> listWithDigits){
+        float totalNumberOfMembers = listWithDigits.size();
+        float totalValue = 0;
+        for (Integer listWithDigit : listWithDigits) {
+            totalValue += listWithDigit;
+        }
+        return totalValue / totalNumberOfMembers;
     }
 }
